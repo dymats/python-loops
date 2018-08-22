4f2b0435538a4c330f9b3e9b8a5a5d4da98afc93
Daphne Matsumoto <dyurita@gmail.com>
2018-08-21 18:04:12 -1000

    finished loops assignment

--- exercises.py
+++ exercises.py
@@ -11,7 +11,8 @@
 # ...
 # ...
 ##'Number 8, your order is ready.'
-
+for num in range(1,9):
+    print('number ' + str(num) + ', your order is ready.')
 
 #2 Now Serving
 #Create a while loop that will print the following message:
@@ -21,7 +22,10 @@
 # ...
 # ...
 # 'Now serving number 5.'
-
+ticket = 1
+while ticket < 6:
+    print('Now serving ' + str(ticket) + '.' )
+    ticket += 1
 
 #3 3 is a Magic Number
 #Create a while loop that will generate a multiplication table for the number 3 and print out the following:
@@ -33,15 +37,23 @@
 # ...
 # ...
 # 9 x 3 = 27
-
+i = 0
+while i < 9:
+    i += 1  
+    print(str(i) + 'x' + str(3) + '=' + str(i * 3))
 
 #4. Uber This!
 # Declare a variable named cars and assign it a list of 5 of your favorite car brands. Next create a for loop that will iterate through the cars list and prints the following: 'My next car will be a red x.' Where x represents each item in the list.
-
+cars = ['Ferrari', 'BMW', 'Mercedes', 'Tesla', 'Porsche' ]
+for car in cars:
+    print('My next car will be a red ' + car + '.')
 
 #5 Uber This Again
 #Print each item in the above cars list using a while loop.
-
+i = 0
+while i < len(cars):
+    print(cars[i])
+    i += 1
     
 #6  No More Tears
 # Create a for loop that will iterate through the cyber attacks list and prints the following: 
@@ -51,35 +63,65 @@
 #The attack at 3 is Krack Attack
 #The attack at 4 is Sambacry.
 #DO NOT use numbers in your string.
-
+ 
 cyber_attacks = ['Wannacry', 'Petya', 'Locky', 'Krack Attack', 'Sambacry']
-
+for attack in cyber_attacks:
+    print('The attack at ' + str(cyber_attacks.index(attack)) + ' is ' + attack + '.')
 
 #7 Even
 # Declare a variable named even_list and assign it an empty list. Next, write a for loop that will place 25 even numbers starting from 0 into the even_list list. Print the even_list variable to see your results. 
-
+even_list = []
+for number in range(1,51):
+    if number %2 == 0:
+        even_list.append(number)
+print(even_list)
 
 #8 Sum Up
 # Create a function named add_up which takes a parameter num. In the code block inside the function, create a variable named sum and assign it a number value of 0. Next, create a for loop that will iterate through a list of numbers using the range function that will be determined by the num parameter and will sum up all the numbers in the list and store it to the sum variable. Print the sum variable to see your results.
 
 #i.e a number list of 10 will have a sum total of 45
-
-
+def add_up(num):
+    sum = 0
+    for number in range(num):
+        sum+= number
+    add_up(10)
+print(sum)    
 #9 East Coast vs West Coast - A Hip Hop Rivalry
 #The East Coast - West Coast hip hop rivalry was a feud between artist and fans of the East Coast hip hop and West Coast hip hop scenes from the mid to last 1990s. 
 
-#Your job is to create a function that will loop through the rappers list and place all the odd indexed items in a list named weessst_side and all the even indexed items in a list named east_side. Print your results.
+#Your job is to create a function that will loop through the rappers list and place all the odd indexed items in a list named west_side and all the even indexed items in a list named east_side. Print your results.
+
 
 rappers = ['Tupac', 'Biggie', 'Ice Cube', 'Nas', 'Snoop', '50 Cent', 'Nate Dogg', 'Wu Tang Clan', 'Kendrick Lamar']
+west_side = []
+east_side = []
+def rivalry(list):
+    for rapper in list:
+        if list.index(rapper)%2 == 0:
+            west_side.append(rapper)
+        else:
+            east_side.append(rapper)
+rivalry(rappers)
+print(west_side)
+print(east_side)            
 
 #10 Breaking Up is Easy
 #Create a for loop that will iterate through 10 even numbers (starting from 0) and stop printing at 10.
+for number in range(0,20,2):
+    if number > 10:
+        break
+    print(number)
 
 
 #11 Zip Codes
 #Create a for loop that will iterate through the zip codes list below and print all the zip codes except for 96822.
 
 zip_codes = [90001,90002,90003,90004,90005,96822,90007,90008,90010,90011,90012,90013,90014,90015, 90016,90017,90018,90019]
+for code in zip_codes:
+    if code == 96822:
+        continue
+        print(code)
+
 
 
 #12 Fizz Buzz!
@@ -98,11 +140,33 @@ zip_codes = [90001,90002,90003,90004,90005,96822,90007,90008,90010,90011,90012,9
 # Buzz
 # ...
 # ...
-# ...
+# ...()
 # 14
 # Fizz Buzz
+for num in range(1,101):
+    if num %3 == 0 and num % 5 == 0:
+        print('Fizz Buzz')
+    elif num %3 ==0:
+        print('Fizz')
+    elif num %5 == 0:
+        print('Buzz')
+    else:
+        print(str(num))
+
 
 
 #13 Fizz Buzz Again 
 #Do the same thing again using a while loop.
+count = 0
+while count < 101:
+    if count %3 ==0 and count %5 == 0:
+        print('Fizz Buzz')
+    elif count %3 == 0:
+        print("Fizz") 
+    elif count %5 == 0:
+        print('Buzz') 
+    else:
+        print(count)
+    count +=1     
+
 
